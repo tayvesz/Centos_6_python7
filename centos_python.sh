8da3ac0777d64e7c9fc7fefa35bcf5fac3d05edb
tayvesz <tayves.zango@gmail.com>
2016-12-26 14:13:03 +0100

    Update centos_python.sh

--- centos_python.sh
+++ centos_python.sh
@@ -7,7 +7,7 @@ yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel
 yum install xz-libs
 wget https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tar.xz
 xz -d Python-2.7.13.tar.xz
-tar -xvf Python-2.7.10.tar
+tar -xvf Python-2.7.13.tar
 cd Python-2.7.13
 ./configure --prefix=/usr/local 
 make
